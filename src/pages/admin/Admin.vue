ae10823d2b14ae65c92dd719d7c1028c5ae246f8
BugCreators <502461775@qq.com>
2019-02-19 14:11:11 +0800

    sidebar fold

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -3,14 +3,16 @@
     <AvueHeader :isAdmin="true" />
     <div class="content">
       <div class="sidebar">
-        <div :class="isCollapse ? `sidebar-width-fold` : ``">
-          <AvueSidebar :isCollapse="isCollapse" :currentComponent="currentComponent" />
-        </div>
+        <AvueSidebar
+          :isCollapse="isCollapse"
+          :currentComponent="currentComponent"
+          @sidebarswitch="sidebarswitch"
+        />
       </div>
-      <div class="displaySidebar" @click="sidebarSwitch">
-        <i class="el-icon-caret-left"></i>
-      </div>
-      <div class="content-right" :class="isCollapse ? `content-right-fold` : ``">
+      <div
+        class="content-right"
+        :class="isCollapse ? 'content-right-fold' : ''"
+      >
         <RouterView />
       </div>
     </div>
@@ -30,7 +32,7 @@ export default {
   data() {
     return {
       isCollapse: false
-    }
+    };
   },
   computed: {
     // 获取当前组件名
@@ -43,7 +45,7 @@ export default {
     }
   },
   methods: {
-    sidebarSwitch() {
+    sidebarswitch() {
       this.isCollapse = !this.isCollapse;
     }
   }
@@ -59,43 +61,19 @@ export default {
   background: @header_bg;
   height: ~"calc(100% - @{header_height})";
   width: @sidebar_width;
-  &-fold {
-    width: @sidebar_width_fold;
-  }
-  &, &-fold {
-    ul {
-      height: 100%;
-      position: fixed;
-    }
-  }
-}
-.displaySidebar{
-  bottom: 0;
-  left: 200px;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  width: 0px;
-  z-index: 10;
-  i {
-    display: block;
-    outline: none;
-    position: absolute;
-    top: 50%;
-    width: 17px;
-    &:active {
-      // transform: rotate(2deg);
-    }
+  ul {
+    height: 100%;
+    position: fixed;
   }
 }
 .content-right {
   overflow: hidden;
-  transition: all .3s;
-    margin-left: @sidebar_width;
-    width: ~"calc(100% - @{sidebar_width})";
-    &-fold {
-      margin-left: @sidebar_width_fold;
-      width: ~"calc(100% - @{sidebar_width_fold})";
-    }
+  transition: all 0.43s;
+  margin-left: @sidebar_width;
+  width: ~"calc(100% - @{sidebar_width})";
+  &-fold {
+    margin-left: @sidebar_width_fold;
+    width: ~"calc(100% - @{sidebar_width_fold})";
+  }
 }
 </style>
--- src/pages/admin/subComponents/AvueSidebar.vue
+++ src/pages/admin/subComponents/AvueSidebar.vue
@@ -5,10 +5,16 @@
     :collapse="isCollapse"
     router
   >
+    <div class="el-submenu__title displaySidebar" @click="sidebarSwitch">
+      <i
+        class="el-icon-d-arrow-left"
+        :class="isCollapse ? 'i-flod' : 'i-no-flod'"
+      ></i>
+    </div>
     <div v-for="(item, index) in $store.state.sidebar" :key="index">
       <el-submenu v-if="'item' in item" :index="subMenuIndex(index)">
         <template slot="title">
-          <i :class="item.class"></i><span slot="title">{{ item.title }}</span>
+          <i :class="item.class"></i><span v-if="!isCollapse" slot="title">{{ item.title }}</span>
         </template>
         <el-menu-item
           v-for="(subItem, subIndex) in item.item"
@@ -43,7 +49,40 @@ export default {
   methods: {
     subMenuIndex(index) {
       return index.toString();
+    },
+    sidebarSwitch() {
+      this.$emit("sidebarswitch");
     }
   }
 };
 </script>
+
+<style lang="less" scoped>
+.flod-animate(@begin, @end, @name) {
+  @keyframes @name {
+    from {
+      transform: rotate(@begin);
+    }
+    to {
+      transform: rotate(@end);
+    }
+  }
+}
+.flod-animate(0, 180deg, flod);
+.flod-animate(180deg, 360deg, no-flod);
+.displaySidebar {
+  position: relative;
+  width: 100%;
+  i {
+    color: white;
+    display: inline-block;
+    height: 14px;
+    left: 5px;
+    position: relative;
+    animation: no-flod .3s forwards;
+    &.i-flod {
+      animation: flod .3s forwards;
+    }
+  }
+}
+</style>
